d23a4f8057be78222ae239ceb902c05162acfd90
mattmuroya <matt.muroya@gmail.com>
2022-04-21 23:13:40 -1000

    add user creation validations

--- server/controllers/userRouter.js
+++ server/controllers/userRouter.js
@@ -10,6 +10,18 @@ userRouter.get('/', async (req, res) => {
 userRouter.post('/', async (req, res) => {
   const { username, name, password } = req.body;
 
+  if (username === undefined || password === undefined) {
+    return res.status(400).json({
+      error: 'Username and password required.'
+    });
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return res.status(400).json({
+      error: 'Username and password must be at least 3 characters.'
+    });
+  }
+
   const existingUsername = await User.findOne({ username });
   if (existingUsername) {
     return res.status(400).json({
